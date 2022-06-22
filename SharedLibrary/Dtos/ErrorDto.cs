db9cac809f323d03c0099a16f90602f447bf2708
huseyinsamsunn <106733276+huseyinsamsunn@users.noreply.github.com>
2022-06-22 18:25:19 +0300

    Add project files.

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+  public  class ErrorDto
+    {
+        public bool IsShow { get; set; }
+        public List<String> Errors { get; private set; }
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(bool isShow, List<string> errors)
+        {
+            IsShow = isShow;
+            Errors = errors;    
+        }
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            isShow = true;
+        }
+
+    }
+}
