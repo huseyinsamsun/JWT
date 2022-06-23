3e09575d242e238bc6fb1042f025bcf0dc8fa002
huseyinsamsunn <106733276+huseyinsamsunn@users.noreply.github.com>
2022-06-23 10:46:50 +0300

    add service

---
+++ AuthServer.Core/Services/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuthServer.Core.Services
+{
+    public interface IUserService
+    {
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByName(string userName);
+        Task<Response<UserAppDto>> GetUserByEmail(string email);
+
+    }
+}
